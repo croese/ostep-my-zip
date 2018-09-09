9f1603e1be122ca11ccf0cc049ae8c95d20ac033
Christian Roese <croese@gmail.com>
2018-09-09 17:40:20 -0500

    Completes my-unzip util

--- my-unzip.c
+++ my-unzip.c
@@ -8,5 +8,32 @@ int main(int argc, char** argv)
     exit(EXIT_FAILURE);
   }
 
+  FILE* fd;
+  int c;
+  int count;
+  size_t nread;
+  for (int i = 1; i < argc; i++) {
+    fd = fopen(argv[i], "r");
+    if (!fd) {
+      perror("fopen");
+      exit(EXIT_FAILURE);
+    }
+
+    while ((nread = fread(&count, sizeof(int), 1, fd)) == 1) {
+      c = fgetc(fd);
+      for (int j = 0; j < count; j++) {
+        printf("%c", c);
+      }
+    }
+
+    if (ferror(fd)) {
+      perror("fread");
+      fclose(fd);
+      exit(EXIT_FAILURE);
+    }
+
+    fclose(fd);
+  }
+
   return 0;
 }
